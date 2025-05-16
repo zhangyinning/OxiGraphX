7251e812db315db2fb6bea807799efb5e0620a76
Yinning Zhang <35968409+zhangyinning@users.noreply.github.com>
2025-05-16 15:40:51 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ This is the class for setting the parameters for dataset and ML training
 class Config:
    # main_path = './'
    
-   dataset_root = '../data'
+   dataset_root = 'data'
    dataset_root2 = 'new_pred'
    # To use multiple compounds' data
    dataset = ['Sr8La24Co7Ni7Mn7Fe7Cr4O96', 'Ba4Sr4La24Co7Ni7Mn7Fe7Cr4O96', 'Ba2Sr6La24Co7Ni7Mn7Fe7Cr4O96',  'Sr8La24Co7Ni7Mn7Fe7Zn4O96', 'Sr32Co7Ni7Mn7Fe7Cr4O96', 'Sr32Co8Ni8Mn8Fe8O96', 'Ba4Sr4La24Co8Ni8Mn8Fe8O96', 'La32Co8Ni8Mn8Fe8O96', 'Sr8La24Co8Ni8Mn8Fe8O96']
